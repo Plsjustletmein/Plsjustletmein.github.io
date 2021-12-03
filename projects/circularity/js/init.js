916fe102b4ed3c7c4fab8c4b6ef5a392efc62d90
Plsjustletmein <stevens20411@students.apsb.org>
2021-12-03 18:25:53 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,27 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+          var circles;
+          var circles = [];
 
-
-        // TODO 2 : Create a function that draws a circle 
-        
+          // TODO 2 : Create a function that draws a circle 
+          circle = draw.randomCircleInArea(canvas, true, true, ‘#999’, 2);
+	      physikz.addRandomVelocity(circle, canvas);
+	      view.addChild(circle);
+	      circles.push(circle);
+ 
 
         // TODO 3 / 8 : Call the drawCircle() function 
-
+        /*drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();*/
+        
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+            drawCircle();
+        }
+        
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,13 +53,26 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+        
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
+            
 
             // TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++) {
+                var eachCircle = circles[i];              
+            }
+
             
         }
     
@@ -62,7 +89,16 @@ var init = function (window) {
             }
             
             // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circle.x < 0) {
+                circle.x = canvas.width;
+            }
+            if (circle.y > canvas.width) {
+                circle.y = 0;
+            }
+            if (circle.y < 0) {
+                circle.y = canvas.height;
+            }
+        
 
 
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
